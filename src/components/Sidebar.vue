05d0d85ddb2f553d960b32569985880a92b8870b
Mickaël Fontaine <m.fontaine@linkbynet.com>
2019-01-18 01:24:37 +0100

    Add Sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="p-2 bg-dark" style="height: 100vh" role="navigation">
+    <router-link
+      to="/"
+      tag="h3"
+      class="text-center my-3"
+      style="cursor: pointer"
+    >
+      <span class="text-light">
+        <span style="font-size: 0.8em" class="align-top">the</span>Good<b-badge
+          style="font-size: 1em"
+          variant="danger"
+          >Tube</b-badge
+        >
+      </span>
+    </router-link>
+    <hr class="text-secondary bg-secondary" />
+    <b-nav vertical>
+      <b-nav-item
+        router-tag="div"
+        class="text-white-50 inactive"
+        active-class="bg-secondary text-light font-weight-bold active"
+        to="/downloading"
+      >
+        <i class="fas fa-arrow-alt-circle-down mr-1"></i>
+        {{ $t("DOWNLOADING") }}
+        <b-badge class="float-right m-1" variant="light">1</b-badge>
+      </b-nav-item>
+      <b-nav-item
+        router-tag="div"
+        class="text-white-50 inactive"
+        active-class="bg-secondary text-light font-weight-bold active"
+        to="/waiting"
+      >
+        <i class="fas fa-clock mr-1"></i> {{ $t("WAITING") }}
+        <b-badge class="float-right m-1" variant="light">0</b-badge>
+      </b-nav-item>
+      <b-nav-item
+        router-tag="div"
+        class="text-white-50 inactive"
+        active-class="bg-secondary text-light font-weight-bold active"
+        to="/stopped"
+      >
+        <i class="fas fa-stop-circle mr-1"></i> {{ $t("STOPPED") }}
+        <b-badge class="float-right m-1" variant="light">0</b-badge>
+      </b-nav-item>
+      <b-nav-item
+        router-tag="div"
+        class="text-white-50 inactive"
+        active-class="bg-secondary text-light font-weight-bold active"
+        to="/finished"
+      >
+        <i class="fas fa-check-circle mr-1"></i> {{ $t("FINISHED") }}
+        <b-badge class="float-right m-1" variant="light">0</b-badge>
+      </b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+.active
+  cursor default
+
+.inactive
+  cursor pointer
+  :hover
+    background-color #6c757d
+</style>
