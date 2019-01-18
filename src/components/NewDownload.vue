2cb153f89551455d99bedd6c934a4f6f04f089bd
Mickaël Fontaine <m.fontaine@linkbynet.com>
2019-01-18 01:25:37 +0100

    Bootstrap NewDownload component

---
+++ src/components/NewDownload.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-2">Here will come the new download form</div>
+</template>
+
+<script>
+export default { name: "NewDownload" };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus"></style>
