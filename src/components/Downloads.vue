b6ef03f1203af94a9c1ddfca9f5012ccfb8c16f6
Mickaël Fontaine <m.fontaine@linkbynet.com>
2019-01-18 02:44:22 +0100

    Downloads: add basic table with mocked data

--- src/components/Downloads.vue
+++ src/components/Downloads.vue
@@ -1,8 +1,40 @@
 <template>
-  <div class="p-2">Downloads will come here (type: {{ downloadsType }}).</div>
+  <div class="m-2">
+    Downloads which are in state "{{ downloadsType }}"<br />
+    <table class="table">
+      <thead>
+        <tr>
+          <th>{{ $t("NAME") }}</th>
+          <th>{{ $t("SIZE") }}</th>
+          <th>{{ $t("PROGRESSION") }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="index" v-for="(download, index) in downloads">
+          <td>{{ download.name }}</td>
+          <td>{{ download.size }}</td>
+          <td>
+            <b-progress
+              :value="download.progress"
+              :max="100"
+              variant="success"
+              show-progress
+              animated
+            ></b-progress>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+const downloads = [
+  { id: 1, name: "first dl", size: "18 Mo", progress: 14 },
+  { id: 2, name: "second dl", size: "1 To", progress: 82 },
+  { id: 3, name: "third dl", size: "5 Go", progress: 42 }
+];
+
 export default {
   name: "Downloads",
   props: {
@@ -10,6 +42,9 @@ export default {
       type: String,
       required: true
     }
+  },
+  data: function() {
+    return { downloads };
   }
 };
 </script>
