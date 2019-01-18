6d53fda51fb81acfed515caea9ee3215a9889f4e
Mickaël Fontaine <m.fontaine@linkbynet.com>
2019-01-18 01:25:53 +0100

    Bootstrap Downloads component

---
+++ src/components/Downloads.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-2">Downloads will come here (type: {{ downloadsType }}).</div>
+</template>
+
+<script>
+export default {
+  name: "Downloads",
+  props: {
+    downloadsType: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus"></style>
