23fa10d3a6e4f39fa13a7e02deffd258dacea38b
M1CK431 <mickael@delight-data.com>
2019-10-19 23:09:54 +0200

    Downloads: get data from API

--- src/components/Downloads.vue
+++ src/components/Downloads.vue
@@ -1,50 +1,53 @@
 <template>
-  <div class="m-2">
-    Downloads which are in state "{{ downloadsType }}"<br />
-    <table class="table">
-      <thead>
-        <tr>
-          <th>{{ $t("NAME") }}</th>
-          <th>{{ $t("SIZE") }}</th>
-          <th>{{ $t("PROGRESSION") }}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr :key="index" v-for="(download, index) in downloads">
-          <td>{{ download.name }}</td>
-          <td>{{ download.size }}</td>
-          <td>
-            <b-progress
-              :value="download.progress"
-              :max="100"
-              variant="success"
-              show-progress
-              animated
-            ></b-progress>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <Loader :loading="loading" class="m-2">
+    <Error :error="error">
+      Downloads which are in state "{{ downloadsType }}"<br />
+      <table class="table">
+        <thead>
+          <tr>
+            <th>{{ $t("NAME") }}</th>
+            <th>{{ $t("SIZE") }}</th>
+            <th>{{ $t("PROGRESSION") }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr :key="index" v-for="(download, index) in downloads">
+            <td>{{ download.name }}</td>
+            <td>{{ download.size }}</td>
+            <td>
+              <b-progress
+                :value="download.progress"
+                :max="100"
+                variant="success"
+                show-progress
+                animated
+              ></b-progress>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div v-show="!downloads.length" class="text-center text-secondary">
+        {{ $t("NO_DOWNLOAD_TO_DISPLAY_HERE") }}
+      </div>
+    </Error>
+  </Loader>
 </template>
 
 <script>
-const downloads = [
-  { id: 1, name: "first dl", size: "18 Mo", progress: 14 },
-  { id: 2, name: "second dl", size: "1 To", progress: 82 },
-  { id: 3, name: "third dl", size: "5 Go", progress: 42 }
-];
+import Loader from "./Loader.vue";
+import Error from "./Error.vue";
 
 export default {
   name: "Downloads",
-  props: {
-    downloadsType: {
-      type: String,
-      required: true
-    }
-  },
-  data: function() {
-    return { downloads };
+  components: { Loader, Error },
+  props: { downloadsType: { type: String, required: true } },
+  data: () => ({ loading: true, error: "", downloads: [] }),
+  mounted() {
+    this.axios
+      .get("http://127.0.0.1:5000/downloads")
+      .then(({ data }) => (this.downloads = data))
+      .catch(error => (this.error = JSON.stringify(error)))
+      .finally(() => (this.loading = false));
   }
 };
 </script>
