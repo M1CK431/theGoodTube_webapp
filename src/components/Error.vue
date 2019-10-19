dd408978662c2fb0a20167d5fcfb2f30c199104f
M1CK431 <mickael@delight-data.com>
2019-10-19 23:09:27 +0200

    Add Error component

---
+++ src/components/Error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="error" class="">
+    <b-alert variant="danger">
+      {{ $t("AN_ERROR_OCCURED") }}
+      {{ $t("SEE_DETAILS_BELOW") }}
+      <br />
+      <b-card bg-variant="light" class="text-center">
+        <b-card-text>{{ error }}</b-card-text>
+      </b-card>
+    </b-alert>
+  </div>
+  <div v-else>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default { props: { error: String } };
+</script>
+
+<style scoped></style>
