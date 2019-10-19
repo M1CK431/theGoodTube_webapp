74842fa091bfb0da69023a45791f99643d694abf
M1CK431 <mickael@delight-data.com>
2019-10-19 23:08:58 +0200

    Add Loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    v-if="loading"
+    class="pt-5 d-flex justify-content-center align-items-center"
+  >
+    <i class="fas fa-spinner fa-spin fa-10x"></i>
+  </div>
+  <div v-else>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default { props: { loading: Boolean } };
+</script>
+
+<style scoped></style>
