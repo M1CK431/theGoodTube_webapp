f98d1cc1570caf78876b90dac757f4b0b36ad15f
M1CK431 <mickael@delight-data.com>
2019-11-09 00:14:38 +0100

    Vuex: add downloads management related functions (+ events handling)

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import axios from "axios";
+import Download from "@/helpers/Download.prototype.js";
 
 Vue.use(Vuex);
 const { VUE_APP_API_URL: apiUrl } = process.env;
 
 export default new Vuex.Store({
   state: { downloads: [], search: "" },
+
   getters: {
     numberOfDownloadsPerStatus: ({ downloads }) =>
       downloads.reduce(
@@ -15,27 +17,103 @@ export default new Vuex.Store({
           [status]: ++acc[status] || 1
         }),
         {}
-      )
+      ),
+    numberOfSelectedDownloadsPerStatus: (_state, { selectedDownloads }) =>
+      selectedDownloads.reduce(
+        (acc, { progress: { status } }) => ({
+          ...acc,
+          [status]: ++acc[status] || 1
+        }),
+        {}
+      ),
+    selectedDownloads: ({ downloads }) =>
+      downloads.filter(({ selected }) => selected)
   },
+
   mutations: {
-    setDownloads: (state, downloads) => (state.downloads = downloads),
+    selectDownload: ({ downloads }, { id, selected }) =>
+      (downloads.find(download => download.id === id).selected = selected),
+    setDownloads: (state, downloads) =>
+      (state.downloads = downloads.map(download => new Download(download))),
     event_progress: (state, payload) => {
       const { downloads } = state;
       const downloadIndex = downloads.findIndex(({ id }) => payload.id === id);
 
       if (downloadIndex > -1) {
-        Object.assign(downloads[downloadIndex], payload);
+        const {
+          selected,
+          progress: { status }
+        } = downloads[downloadIndex];
+
+        Object.assign(downloads[downloadIndex], {
+          ...new Download(payload),
+          // Preserve selected state except if status change
+          selected: payload.progress.status === status ? selected : false
+        });
       } else {
-        downloads.push(payload);
+        downloads.push(new Download(payload));
       }
       state.downloads = downloads;
     },
+    get event_start() {
+      return this.event_progress;
+    },
+    get event_stop() {
+      return this.event_progress;
+    },
+    event_clear: ({ downloads }, { id }) => {
+      const downloadIndex = downloads.findIndex(download => download.id === id);
+
+      downloads.splice(downloadIndex, 1);
+    },
+    get event_delete() {
+      return this.event_clear;
+    },
     setSearch: (state, search) => (state.search = search)
   },
+
   actions: {
     getDownloads: ({ commit }) =>
       axios
         .get(`${apiUrl}/downloads`)
-        .then(({ data }) => commit("setDownloads", data))
+        .then(({ data }) => commit("setDownloads", data)),
+    clearFinishedDownloads: ({ state: { downloads } }) =>
+      axios.all(
+        downloads
+          .filter(({ progress: { status } }) => status === "finished")
+          .map(({ id }) => axios.delete(`${apiUrl}/downloads/${id}`))
+      ),
+    startSelectedDownloads: ({ state: { downloads } }) =>
+      axios.all(
+        downloads
+          .filter(
+            ({ selected, progress: { status } }) =>
+              selected && status === "stopped"
+          )
+          .map(({ id }) =>
+            axios.patch(`${apiUrl}/downloads/${id}/progress`, {
+              status: "start"
+            })
+          )
+      ),
+    stopSelectedDownloads: ({ state: { downloads } }) =>
+      axios.all(
+        downloads
+          .filter(
+            ({ selected, progress: { status } }) =>
+              selected && status === "downloading"
+          )
+          .map(({ id }) =>
+            axios.patch(`${apiUrl}/downloads/${id}/progress`, {
+              status: "stop"
+            })
+          )
+      ),
+    deleteSelectedDownloads: ({ state: { downloads } }) =>
+      axios.all(
+        downloads
+          .filter(({ selected }) => selected)
+          .map(({ id }) => axios.delete(`${apiUrl}/downloads/${id}/files`))
+      )
   }
 });
